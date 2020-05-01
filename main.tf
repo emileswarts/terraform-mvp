aa0b651476832b844091455849727759106ea401
Emile <emile.swarts@digital.justice.gov.uk>
2020-05-01 14:52:35 +0100

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "> 0.12.0"
+}
+
+provider "aws" {
+  version = "~> 2.52"
+}
+
+provider "random" {
+  version = "~> 2.2.1"
+}
+
+resource "random_string" "random" {
+  length = 10
+  upper = false
+  special = false
+}
+
+resource "aws_s3_bucket" "poc" {
+  bucket = random_string.random.result
+  acl    = "private"
+}
+
