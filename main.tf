4448761824d47c90a48bb4430300e4d07cfdf14a
Emile <emile.swarts@digital.justice.gov.uk>
2020-06-11 14:14:06 +0100

    Use local S3 backend

--- main.tf
+++ main.tf
@@ -5,23 +5,20 @@ terraform {
 provider "aws" {
   version = "~> 2.52"
   region  = "eu-west-2"
+}
+
+provider "aws-production" {
+  version = "~> 2.52"
+  region  = "eu-west-2"
 
   assume_role {
-     role_arn = "arn:aws:iam::899987639597:role/cross-account-pttp"
+    role_arn = "arn:aws:iam::899987639597:role/cross-account-pttp"
   }
 }
 
-provider "random" {
-  version = "~> 2.2.1"
-}
-
-resource "random_string" "random" {
-  length = 10
-  upper = false
-  special = false
-}
-
-resource "aws_s3_bucket" "poc" {
-  bucket = "pttp-showcase-${random_string.random.result}"
-  acl    = "private"
+module "s3" {
+  source    = "./modules/s3"
+  providers = {
+    aws = "aws-production"
+  }
 }
