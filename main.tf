7453b2519f96b97890fd667551a5c4f1a6910421
Emile <emile.swarts@digital.justice.gov.uk>
2020-06-18 15:03:12 +0100

    Provider is now in the buildspec file

--- main.tf
+++ main.tf
@@ -2,21 +2,21 @@ terraform {
   required_version = "> 0.12.0"
 }
 
-provider "aws" {
-  version = "~> 2.52"
-  region  = "eu-west-2"
-}
-
-provider "aws" {
-  alias = "production"
-  version = "~> 2.52"
-  region  = "eu-west-2"
-
-  assume_role {
-    role_arn = "arn:aws:iam::068084030754:role/SharedServicesCodeBuild"
-  }
-}
-
+//provider "aws" {
+//  version = "~> 2.52"
+//  region  = "eu-west-2"
+//}
+//
+//provider "aws" {
+//  alias = "production"
+//  version = "~> 2.52"
+//  region  = "eu-west-2"
+//
+//  assume_role {
+//    role_arn = "arn:aws:iam::068084030754:role/SharedServicesCodeBuild"
+//  }
+//}
+//
 module "s3" {
   source    = "./modules/s3"
 }
